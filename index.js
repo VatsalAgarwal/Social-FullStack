8613f14e8956755c8b8705365d7818c1653114fc
VatsalAgarwal <vatsal0298@gmail.com>
2023-05-10 00:57:20 +0530

    off to bed

--- index.js
+++ index.js
@@ -9,7 +9,19 @@ import multer from "multer";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
+import { appendFile } from "fs";
 
 /* MIDDLEWARE */
 
 const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+dotenv.config();
+const express = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy( { policy: "cross-origin"}));
+app.use(morgan("common"));
+app.use(bodyParser.json({limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
+app.use(cors());
+app.use("/assets", express.static(path.join(__dirname, 'public/assets')));
