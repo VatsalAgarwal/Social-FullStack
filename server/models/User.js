c00efe46daeff55fce5f9b2306994466ff1effde
VatsalAgarwal <vatsal0298@gmail.com>
2023-05-22 17:20:40 +0530

    Models

---
+++ server/models/User.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        require: true,
+        min: 2,
+        max: 50,
+    },
+    lastName: {
+        type: String,
+        require: true,
+        min: 2,
+        max: 50,
+    },
+    email: {
+        type: String,
+        require: true,
+        unique: true,
+        max: 50,
+    }
+})
